07c2ea84804ed2beb2699556955b58a0f3f2a6ba
Marcelo Araújo <marcelo_araujo28@hotmail.com>
2026-03-22 17:11:40 -0300

    refactor: added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import Fastify from "fastify";
 import multipart from "@fastify/multipart";
+import cors from "@fastify/cors";
 import { extratosRoutes } from "./routes/extratos.routes";
 
 export function buildApp() {
-  const app = Fastify();
-
+  const app = Fastify({
+    logger: true,
+  });
+  app.register(cors, {
+    origin: "*",
+  });
   // plugin para upload de arquivos
   app.register(multipart);
   // registrar rota de extratos
